fd0ad5ea3dbbe674028895f4d527e922d2ad0fe6
pkv <pkv@gmail.com>
2019-02-11 21:34:13 +0530

    editedv

--- src/shell_project/core.clj
+++ src/shell_project/core.clj
@@ -4,5 +4,5 @@
   "I don't do a whole lot."
   [x y z]
   (println x "vikram")
-  (println y "This is to check anything")
+  (println y "edited")
   (println z "Have not added a nwfile but edited the existing file"))
